ae8c2aa9c99f78d09326e13a80ee932a6e86aa9f
ash-tam-06 <30047609@cc.peralta.edu>
2025-02-01 15:27:10 -0800

    Add files via upload

---
+++ Simple_Calculator/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    char operation;
+    short num1;
+    short num2;
+    cout << "What operation would you like to use? +? -? *? /?" << endl;
+    cin >> operation;
+    cout << "What is the first number?" << endl;
+    cin >> num1;
+    cout << "What is the second number?" << endl;
+    cin >> num2;
+    if (operation == '/') {
+        while (num2 == 0) {
+            cout << "Please chose a non zero number" << endl;
+            cin >> num2;
+        }
+    }
+    switch (operation) {
+        case '+':
+            cout << num1 + num2 << endl;
+            break;
+        case '-':
+            cout << num1 - num2 << endl;
+            break;
+        case '*':
+            cout << num1 * num2 << endl;
+            break;
+        case '/':https://github.com/ash-tam-06/Test.git
+            cout << num1 / num2 << endl;
+            break;
+        default:
+            cout << "Invalid operation." << endl;
+    }
+
+    return 0;
+}
+
+// TIP See CLion help at <a
+// href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
+//  Also, you can try interactive lessons for CLion by selecting
+//  'Help | Learn IDE Features' from the main menu.
